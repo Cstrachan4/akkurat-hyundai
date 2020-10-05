a89095de1c543f12519484589fafbc2567a8f370
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-10-05 14:09:46 +0100

    Add text fade on inview

--- public/components/Text/index.js
+++ public/components/Text/index.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.scss';
 import clsx from 'clsx';
 
 import Div100vh from 'react-div-100vh';
+import { Waypoint } from 'react-waypoint';
 
 export default function Text({
   lineOne,
   lineTwo,
   align
 }) {
-
+  const [display, setDisplay] = useState(false);
+  const onEnter = () => {
+    setDisplay(true);
+  }
+  const onExit = () =>  {
+    setDisplay(false);
+  }
   return (
     <Div100vh
       className={clsx(
         'text',
-        align && `text--${align}`
+        align && `text--${align}`,
+        display && 'text--display'
       )}
     >
       <div>
+        <Waypoint onEnter={onEnter} onLeave={onExit} topOffset="10%"/>
         <div>
        	  {lineOne}
         </div>
         <div>
           {lineTwo}
         </div>
+
       </div>
     </Div100vh>
   )

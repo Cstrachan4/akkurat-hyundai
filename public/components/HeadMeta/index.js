6013cfff288736e92b35d3d9f3ab326490d49222
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-10-14 12:02:36 +0100

    Add missing edits from last commit

--- public/components/HeadMeta/index.js
+++ public/components/HeadMeta/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head';
 import FavIcon from 'assets/favicon/favicon.js';
 
-const HeadMeta = ({title, description}) => {
+const HeadMeta = ({title, description, shareImage}) => {
 
 return(
   <Head>
- 	<meta charSet="utf-8" />
- 	<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
     <FavIcon />
     <title>{ title || '' }</title>
     <meta key="description" name="description" content={description} />
+    <meta property="og:title" content={title}>
+    <meta property="og:description" content={description}>
+    <meta property="og:image" content={shareImage}>
   </Head>
 )}
 

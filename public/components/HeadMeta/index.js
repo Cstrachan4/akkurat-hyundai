0d57ac560aa7d98d5c6807bce076196624a51bc0
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-11-02 10:25:06 +0000

    Add extra meta tags and implement gtag

--- public/components/HeadMeta/index.js
+++ public/components/HeadMeta/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import FavIcon from 'assets/favicon/favicon.js';
+import Gtag from '../Gtag';
+
+import { SITE_URL } from 'lib/constants.js';
 
 const HeadMeta = ({title, description, shareImage}) => {
 
@@ -11,8 +14,12 @@ return(
     <title>{ title || '' }</title>
     <meta key="description" name="description" content={description} />
     <meta property="og:title" content={title} />
+    <meta property="og:url" content={SITE_URL} />
+    <meta property="og:site_name" content={title || ''} />
+    <meta property="og:type" content="website" />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={shareImage} />
+    <Gtag trackingId="G-00WYR5N8L1"/>
   </Head>
 )}
 
---
+++ public/lib/constants.js
@@ -0,0 +1,1 @@
+export const SITE_URL = 'https://latest.akkurat.tv'

406dcf01d062078d65add143818ea04002430449
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-11-02 10:43:48 +0000

    Try to fix gtag id

--- public/components/HeadMeta/index.js
+++ public/components/HeadMeta/index.js
@@ -19,7 +19,7 @@ return(
     <meta property="og:type" content="website" />
     <meta property="og:description" content={description} />
     <meta property="og:image" content={shareImage} />
-    <Gtag trackingId="251041961"/>
+    <Gtag trackingId="G-00WYR5N8L1"/>
   </Head>
 )}
 
